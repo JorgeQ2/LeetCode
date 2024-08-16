2d5b55ad0d0c8fb4534585d66f07ed01b0793fbc
Jorge Quintero <175043690+JorgeQ2@users.noreply.github.com>
2024-08-16 09:51:17 -0500

    finsihed day 3

---
+++ LeetCode/HashMap/WordPatterns.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashMap
+{
+    public class WordPatterns
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            string[] words = s.Split(' ');
+            if (pattern.Length != words.Length) return false;
+
+            Dictionary<char, string> charToWord = new Dictionary<char, string>();
+            Dictionary<string, char> wordToChar = new Dictionary<string, char>();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                char c = pattern[i];
+                string word = words[i];
+
+                if (charToWord.ContainsKey(c))
+                {
+                    if (charToWord[c] != word) return false;
+                }
+                else
+                {
+                    charToWord[c] = word;
+                }
+
+                if (wordToChar.ContainsKey(word))
+                {
+                    if (wordToChar[word] != c) return false;
+                }
+                else
+                {
+                    wordToChar[word] = c;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
