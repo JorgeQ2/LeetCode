bf48e2e1928b0d6abb0b9f386ad6c7205af4001e
Jorge Quintero <175043690+JorgeQ2@users.noreply.github.com>
2024-08-19 18:02:58 -0500

    day 4

---
+++ LeetCode/HashMap/HappyNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashMap
+{
+    public class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            HashSet<int> saw = new HashSet<int>();
+            saw.Add(n);
+            while (n != 1 && !saw.Contains(n))
+            {
+                saw.Add(n);
+                n = sumOfSquares(n);
+            }
+            return n == 1;
+
+        }
+        private int sumOfSquares(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                sum += (int)Math.Pow(digit, 2);
+                n /= 10;
+
+            }
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class TwoSums
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            HashSet<int> set = new HashSet<int>();
+            //used for loop
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                //check for compliment
+                if (set.Contains(complement))
+                {
+                    //if exist we return 
+                    return new int[] { Array.IndexOf(nums, complement), i };
+                }
+                //add to hash  if not there
+                set.Add(nums[i]);
+            }
+            throw new ArgumentException("No two sum solution");
+        }
+
+    }
+}
