dd84d8aede9f16f918854453ed7f9ec3d019f4cd
Jorge Quintero <175043690+JorgeQ2@users.noreply.github.com>
2024-08-20 19:14:15 -0500

    day5

---
+++ LeetCode/BinaryTree/InvertTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BinaryTree
+{
+    public class InvertTrees
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            // Swap 
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}
---
+++ LeetCode/LinkedList/ListCycle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace LeetCode.LinkedList
+//{
+    //public class ListCycle
+  //  {
+  //      //public bool HasCycle(ListNode head)
+////        {
+////       
+////            {
+////                if (head == null) return false;
+////                ListNode tortoise = head;
+////                ListNode hare = head;
+////
+////                while (hare != null && hare.next != null)
+////                {
+////                    hare = hare.next.next;
+////                    tortoise = tortoise.next;
+////                    if (hare == tortoise) return true;
+////                }
+////                return false;
+////            }
+////        }
+////
+////
+////    }
+////
+//}
+//
