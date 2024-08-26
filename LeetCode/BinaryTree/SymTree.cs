7bbe35f7d49ef84f20364b381e09d8b789e76ff1
Jorge Quintero <175043690+JorgeQ2@users.noreply.github.com>
2024-08-26 11:13:56 -0500

    removed all test

--- LeetCode/BinaryTree/SymTree.cs
+++ LeetCode/BinaryTree/SymTree.cs
@@ -22,18 +22,18 @@ namespace LeetCode.BinaryTree
         }
     }
 
-    // Definition for a binary tree node.
-   // public class TreeNode
-   // {
-   //     public int val;
-   //     public TreeNode left;
-   //     public TreeNode right;
-   //     public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-   //     {
-   //         this.val = val;
-   //         this.left = left;
-   //         this.right = right;
-   //     }
-   // }
+  // Definition for a binary tree node.
+   public class TreeNode
+   {
+       public int val;
+       public TreeNode left;
+       public TreeNode right;
+       public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+       {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
 
 }
