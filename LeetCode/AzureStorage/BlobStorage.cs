c3a87e8bbadb3d3cf9edf4ef9e17a63f0d864707
Jorge Quintero <175043690+JorgeQ2@users.noreply.github.com>
2024-08-28 09:54:17 -0500

    day9 updated

--- LeetCode/AzureStorage/BlobStorage.cs
+++ LeetCode/AzureStorage/BlobStorage.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 //        BlobServiceClient client;
 //        public BlobStorage()
 //        {
-//            client = new BlobServiceClient(@"DefaultEndpointsProtocol=https;AccountName=vc0316;AccountKey=EtncGrpA7RkLfhOz5WuaA57bZ7jR4Gx/TgYfmGidUxfJL1qNdiTxnoca6GPK24Qi4NWUmFk5Mib2+AStC/IjOA==;EndpointSuffix=core.windows.net");
+//
 //        }
 //        public void CreateFolder(string folderName)
 //        {
